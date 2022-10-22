b4a3c2730dbd020e3480688cf6756960f5e0807a
Михайлова Валерия Романовна <114227035+valerrron23@users.noreply.github.com>
2022-10-22 18:45:07 +0300

    Add files via upload

---
+++ 3zad 22.10. 12variant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+        Console.WriteLine("Введите X="); //программа просит пользователя ввести переменную вещественного типа
+        int x = int.Parse(Console.ReadLine()); //в программу введены данные
+
+        Console.WriteLine("Введите Y="); //программа просит пользователя ввести переменную вещественного типа
+
+        int y = int.Parse(Console.ReadLine()); //в программу введены данные
+
+        if (x > 40 && x > -40 && y > 40 && y > -40) //если 
+            Console.WriteLine("Точка лежит в заштрихованной области"); //вывод данных
+        else if (x == 40 || y == 40 || x == -40 || y == -40) //иначе 
+            Console.WriteLine("Точка лежит на границе области"); //вывод данных
+        else
+            Console.WriteLine("Точка НЕ лежит в заштрихованной области"); //вывод данных
+        }
+    }
+}
